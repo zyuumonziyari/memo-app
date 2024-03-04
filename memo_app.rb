1e89d4e3b2139ca677d6d98fd28d44d2154e01cc
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-03-04 23:11:36 +0900

    データ格納をuuidをキーにしたハッシュに変更、メソッド名の修正,

--- memo_app.rb
+++ memo_app.rb
@@ -12,40 +12,43 @@ not_found do
   'ページが見つかりません!'
 end
 
-def load_memos
+def load_memo
   JSON.parse(File.read(MEMOS_FILE), symbolize_names: true)
 end
 
-def add_memos(params)
-  existing_data = load_memos
-  existing_data << { id: SecureRandom.uuid, title: params[:title], content: params[:content], created_at: Time.now, updated_at: Time.now }
-  save_memos(existing_data)
+def add_memo(params)
+  existing_data = load_memo
+  existing_data[SecureRandom.uuid] = { title: params[:title], content: params[:content], created_at: Time.now, updated_at: Time.now }
+  save_memo(existing_data)
 end
 
-def update_memos(params)
-  existing_data = load_memos
-  updating_data = existing_data.find { |memo| memo[:id] == params[:id] }
-  updating_data.merge!(title: params[:title], content: params[:content], updated_at: Time.now)
-  save_memos(existing_data)
+def update_memo(params, memo, existing_data)
+  memo.merge!(title: params[:title], content: params[:content], updated_at: Time.now)
+  save_memo(existing_data)
 end
 
-def delete_memos(params)
-  existing_data = load_memos
-  destroyed_data = existing_data.reject! { |memo| memo[:id] == params[:id] }
-  save_memos(destroyed_data)
+def delete_memo(params)
+  existing_data = load_memo
+  existing_data.delete(params[:id].to_sym)
+  save_memo(existing_data)
 end
 
-def save_memos(indicated_data)
-  File.write(MEMOS_FILE, JSON.pretty_generate(indicated_data))
+def save_memo(existing_data)
+  File.write(MEMOS_FILE, JSON.pretty_generate(existing_data))
 end
 
 def find_memo(params)
-  existing_data = load_memos
-  existing_data.find { |memo| memo[:id] == params }
+  existing_data = load_memo
+  memo = existing_data[params[:id].to_sym]
+  if memo && params[:title] || params[:content]
+    update_memo(params, memo, existing_data)
+  else
+    memo
+  end
 end
 
 get '/' do
-  @memos = load_memos
+  @memos = load_memo
   erb :index
 end
 
@@ -54,12 +57,12 @@ get '/new' do
 end
 
 post '/create' do
-  add_memos(params)
+  add_memo(params)
   redirect '/'
 end
 
 get '/:id' do
-  @memo = find_memo(params[:id])
+  @memo = find_memo(params)
   if @memo.nil?
     status 404
   else
@@ -68,17 +71,17 @@ get '/:id' do
 end
 
 get '/:id/edit' do
-  @memo = find_memo(params[:id])
+  @memo = find_memo(params)
   erb :edit
 end
 
 patch '/:id/update' do
-  update_memos(params)
+  find_memo(params)
   redirect "/#{params[:id]}"
 end
 
 delete '/:id/destroy' do
-  delete_memos(params)
+  delete_memo(params)
   redirect '/'
 end
 
